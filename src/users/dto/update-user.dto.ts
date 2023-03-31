0297fbb8c46510896e5c9f304a393c38c8ea7c1a
Yuri Pires <yuripiresbr@gmail.com>
2023-03-31 07:43:37 -0300

    Updated update-user.dto.ts

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,24 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsString, IsEmail, IsEnum } from 'class-validator';
+import { UserRole } from '../constants/user-role.constant';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsEnum(UserRole)
+  role: UserRole;
+}
