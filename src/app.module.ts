c8adbf283b773e51144039a2a02ef8d32670b214
Yuri Pires <yuripiresbr@gmail.com>
2023-06-02 21:21:07 -0300

    refactor: added env

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,22 +20,33 @@ import { CalendarModule } from './calendar/calendar.module';
 import { CalendarDayModule } from './calendar-day/calendar-day.module';
 import { Calendar } from './calendar/entities/calendar.entity';
 import { CalendarDay } from './calendar-day/entities/calendar-day.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: 'postgresql://postgres:fNGyDxO4vdXyyKBjpOfc@containers-us-west-78.railway.app:5878/railway',
-      entities: [
-        User,
-        Course,
-        Matrix,
-        Subject,
-        Professor,
-        Calendar,
-        CalendarDay,
-      ],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        console.log(configService.get<string>('DB_CONNECTION'));
+        return {
+          type: 'postgres',
+          url: configService.get<string>('DB_CONNECTION'),
+          entities: [
+            User,
+            Course,
+            Matrix,
+            Subject,
+            Professor,
+            Calendar,
+            CalendarDay,
+          ],
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
     }),
     UsersModule,
     AuthModule,
